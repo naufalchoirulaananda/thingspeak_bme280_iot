06362a49c1adde268e075d5986dd4d3af377c623
naufalchoirulaananda <naufalchoirulananda@zohomail.com>
2024-05-26 20:44:57 +0700

    finish

---
+++ thingspeak_bme280.ino
@@ -0,0 +1,66 @@
+#include <WiFi.h>
+#include "ThingSpeak.h"
+#include <Adafruit_BME280.h>
+#include <Adafruit_Sensor.h>
+
+const char* ssid = "spontan.";
+const char* password = "uuuhhhuuuyyy";
+
+WiFiClient  client;
+
+unsigned long myChannelNumber = 1;
+const char * myWriteAPIKey = "8MDBYSO3CA5ENYC6";
+
+// Timer variables
+unsigned long lastTime = 0;
+unsigned long timerDelay = 10000;
+
+// Variable to hold temperature readings
+float temperatureC;
+Adafruit_BME280 bme;
+
+void initBME(){
+  if (!bme.begin(0x76)) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);  //Initialize serial
+  initBME();
+  
+  WiFi.mode(WIFI_STA);   
+  
+  ThingSpeak.begin(client);  // Initialize ThingSpeak
+}
+
+void loop() {
+  if ((millis() - lastTime) > timerDelay) {
+    
+    // Connect or reconnect to WiFi
+    if(WiFi.status() != WL_CONNECTED){
+      Serial.print("Attempting to connect");
+      while(WiFi.status() != WL_CONNECTED){
+        WiFi.begin(ssid, password); 
+        delay(5000);     
+      } 
+      Serial.println("\nConnected.");
+    }
+
+    // Get a new temperature reading
+    temperatureC = bme.readTemperature();
+    Serial.print("Temperature (ºC): ");
+    Serial.println(temperatureC);
+
+    int x = ThingSpeak.writeField(myChannelNumber, 1, temperatureC, myWriteAPIKey);
+
+    if(x == 200){
+      Serial.println("Channel update successful.");
+    }
+    else{
+      Serial.println("Problem updating channel. HTTP error code " + String(x));
+    }
+    lastTime = millis();
+  }
+}
